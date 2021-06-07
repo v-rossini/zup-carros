cceaf205f641a006cab92cc58ffaf1bc414e9892
Vitor Rossini <rossinivitor@hotmail.com>
2021-06-07 14:40:00 -0300

    Projeto criado

---
+++ api/src/main/java/br/com/vitor/zupcarros/ZupcarrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitor.zupcarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupcarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupcarrosApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/br/com/vitor/zupcarros/ZupcarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitor.zupcarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupcarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
