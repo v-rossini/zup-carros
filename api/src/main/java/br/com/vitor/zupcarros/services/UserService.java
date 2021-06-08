6b5cd12e74ac7ecaf25bcbc409f157974ff24640
Vitor Rossini <rossinivitor@hotmail.com>
2021-06-08 02:42:36 -0300

    UserService

--- api/src/main/java/br/com/vitor/zupcarros/services/UserService.java
+++ api/src/main/java/br/com/vitor/zupcarros/services/UserService.java
@@ -1,9 +1,19 @@
 package br.com.vitor.zupcarros.services;
 
+import java.text.ParseException;
+
+import javax.mail.internet.AddressException;
+
+import org.springframework.http.ResponseEntity;
+
+import br.com.vitor.zupcarros.controller.forms.UserForm;
+import br.com.vitor.zupcarros.dto.UserDto;
 import br.com.vitor.zupcarros.entities.User;
 
 public interface UserService {
 
 	User findUser(Long userId);
+    ResponseEntity<UserDto> registerUser(UserForm form) throws AddressException, ParseException;
+    ResponseEntity<UserDto> getUser(Long id);
 
 }
--- api/src/main/java/br/com/vitor/zupcarros/services/UserServiceImp.java
+++ api/src/main/java/br/com/vitor/zupcarros/services/UserServiceImp.java
@@ -1,14 +1,52 @@
 package br.com.vitor.zupcarros.services;
 
 
+import java.text.ParseException;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import javax.mail.internet.AddressException;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import br.com.vitor.zupcarros.controller.forms.UserForm;
+import br.com.vitor.zupcarros.dto.UserDto;
 import br.com.vitor.zupcarros.entities.User;
+import br.com.vitor.zupcarros.repositories.UserRepository;
+import br.com.vitor.zupcarros.services.util.ServicesUtil;
 
 public class UserServiceImp implements UserService {
+	
+	@Autowired
+	UserRepository repository;
+	@Autowired
+	ServicesUtil util;
+	
 
+	public User findUser(@NotNull @NotEmpty @NotBlank Long userId) {
+		Optional<User> user = repository.findById(userId);
 
-	public User findUser(Long userId) {
-		User user = new User();
-		return user;
+		if (!user.isPresent()) 
+			throw new NoSuchElementException("Não foi encontrado um usuário com o id: " + userId.toString());
+		
+		return user.get();
+		
+	}
+
+	public ResponseEntity<UserDto> registerUser(UserForm form) throws AddressException, ParseException {
+		User user = repository.save(form.convertToEntity(util));
+		return new ResponseEntity<>(new UserDto(user), HttpStatus.CREATED);
+		
+
+	}
+
+	public ResponseEntity<UserDto> getUser(Long userId) {
+		return ResponseEntity.ok(new UserDto(findUser(userId)));
 	}
 
 }
